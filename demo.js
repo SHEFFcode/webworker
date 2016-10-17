3017cdbbdd88841d8a851916448879720856363f
SHEFFcode <sheff@sheffmachine.com>
2016-10-16 21:37:28 -0700

    web worker

---
+++ demo.js
@@ -0,0 +1,8 @@
+//receive messages
+self.onmessage = function (e) {
+  var messageObject = e.data;
+  var message = messageObject.messagekey;
+  setTimeout(function () {
+    self.postMessage(message + 'Processed')}, 1000);
+
+};
